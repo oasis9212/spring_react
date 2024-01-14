3df91e8b6bc0c9ece2d55d6620bb84d98723e7c7
lostfly2 <lostfly2@naver.com>
2024-01-14 15:10:02 +0900

    완성 !! ㅅㅂ.

--- src/main/java/com/example/spring_react/SpringReactApplication.java
+++ src/main/java/com/example/spring_react/SpringReactApplication.java
@@ -2,8 +2,10 @@ package com.example.spring_react;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(value = "com")
 public class SpringReactApplication {
 
 	public static void main(String[] args) {
---
+++ src/test/java/com/rest/apitescontroller.java
@@ -0,0 +1,13 @@
+package com.rest;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class apitescontroller {
+    @GetMapping("api/demo")
+    public String hello(){
+        return "jeon hello";
+    }
+}
